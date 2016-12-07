ec95978a8954df17484eafc179cfa3dd8793b385
SajayBailey <sajaymarsh@gmail.com>
2016-12-06 23:34:32 -0500

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,36 @@
+-- Create CheapoMail database
+
+DROP DATABASE IF EXISTS cheapomail;
+CREATE DATABASE cheapomail;
+USE cheapomail;
+
+-- Creating User table
+
+CREATE TABLE User (
+id int NOT NULL UNIQUE auto_increment,
+firstname varchar(255) NOT NULL,
+lastname varchar(255) NOT NULL,
+username varchar(255) NOT NULL,
+password varchar(255) NOT NULL
+);
+
+-- Create Message table
+CREATE TABLE Message (
+id int NOT NULL UNIQUE auto_increment,
+recipient_ids int NOT NULL UNIQUE,
+user_id int NOT NULL UNIQUE,
+subject varchar(255),
+message_body TEXT,
+date_sent DATE NOT NULL
+);
+
+-- Message read table
+CREATE TABLE Message_read (
+id int NOT NULL UNIQUE,
+message_id int NOT NULL UNIQUE,
+reader_id int NOT NULL UNIQUE,
+date_read DATE NOT NULL
+);
+
+-- Test User
+-- INSERT INTO User VALUES ("John", "Doe", "johnny", "password");
